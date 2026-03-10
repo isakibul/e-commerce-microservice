53dcc73c05487a9900c624b9aa5117e7bd222a27
isakibul <isakibul108@gmail.com>
2026-03-10 19:33:27 +0600

    feat: inventory application setup

---
+++ services/inventory/src/index.ts
@@ -0,0 +1,46 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/health", (_req, res) => {
+  res.status(200).json({ status: "Up" });
+});
+
+/**
+ * 404 handler
+ */
+app.use((_req, res) => {
+  res.status(404).json({ error: "Not Found" });
+});
+
+/**
+ * Error handler
+ */
+app.use(
+  (
+    err: Error,
+    _req: express.Request,
+    res: express.Response,
+    _next: express.NextFunction,
+  ) => {
+    console.error(err.stack);
+    res.status(500).json({ error: "Internal Server Error" });
+  },
+);
+
+const PORT = process.env.PORT || 4002;
+const serviceName = process.env.SERVICE_NAME || "Inventory Service";
+
+app.listen(PORT, () => {
+  console.log(`${serviceName} is running on port ${PORT}`);
+});
