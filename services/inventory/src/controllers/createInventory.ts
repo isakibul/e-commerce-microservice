59aa2eff5bd93b46933f78dfd97424cdefc74746
isakibul <isakibul108@gmail.com>
2026-03-13 21:41:52 +0600

    feat: create inventory controller

---
+++ services/inventory/src/controllers/createInventory.ts
@@ -0,0 +1,45 @@
+import { prisma } from "@/prisma";
+import { InventoryCreateDTOSchema } from "@/schemas";
+import { NextFunction, Request, Response } from "express";
+
+const createInventory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    /**
+     * Validate request body
+     */
+    const parsedBody = InventoryCreateDTOSchema.safeParse(req.body);
+
+    if (!parsedBody.success) {
+      return res.status(400).json({ error: parsedBody.error.message });
+    }
+
+    /**
+     * Create inventory
+     */
+    const inventory = await prisma.inventory.create({
+      data: {
+        ...parsedBody.data,
+        histories: {
+          create: {
+            actionType: "In",
+            quantityChanged: parsedBody.data.quantity,
+            lastQuantity: 0,
+            newQuantity: parsedBody.data.quantity,
+          },
+        },
+      },
+      select: {
+        id: true,
+        quantity: true,
+      },
+    });
+
+    return res.status(201).json(inventory);
+  } catch (error) {
+    next(error);
+  }
+};
